24624d3c18547b4c1bd467cf9ab4f0f5308ff9bd
st3ll44n <stellanaziozeno@gmail.com>
2024-11-14 16:30:48 -0300

    Aula 1 - Introdução ao JavaScript

---
+++ AULA 1 - Introdução/scripts/somar.js
@@ -0,0 +1,14 @@
+var n1 = prompt('Digite o primeiro número')
+var n2 = prompt('Digite o segundo número')
+var soma = Number(n1) + Number(n2)
+
+alert('A soma dos números é de: '+soma)
+
+// Number () - transfroma texto em número 
+
+// var - global e sobrescrevivel
+var variavel_global = 1
+// let - global e sobrescrevivel 
+let variavel_local = 3 
+// const - global e sobrescrevivel
+const variavel_constante = "fixo"
---
+++ AULA 1 - Introdução/scripts/teste.js
@@ -0,0 +1,18 @@
+// caixa de texto 
+// alert('Hello World')
+// var nome = prompt('Qual é o seu nome?')
+// confirm('Deseja baixar novamente?')
+
+// tipos de variavel 
+// strimg --> caractere
+// number --> numero (inteiro, real)
+// boolean --> logico (verdadeiro e falso)
+// array - vetor (lista)
+var nome = prompt('Qual é o seu nome?')
+var numero_fav = 51
+var ligado = true
+var valor_total = 1.7
+var lista = []
+
+var aniversario = prompt('Qual é o dia do seu aniversário ?')
+alert('Olá '+nome+' o dia do seu aniversário é '+aniversario)
