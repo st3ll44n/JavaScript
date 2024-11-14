6d26b0b02f551d9d242ee7a9f37b74b10b14b8f0
st3ll44n <stellanaziozeno@gmail.com>
2024-11-14 17:00:32 -0300

    Atividades - Aula 1

---
+++ AULA 1 - Introdução/atividade/scripts/antecessor.js
@@ -0,0 +1,5 @@
+var n1 = prompt("Digite um número e descubra o seu antecessor e sucessor!")
+var n2 = Number(n1) - 1 
+var n3 = Number(n1) + 1
+
+alert('O sucessor de '+n1+' é '+n3+' e seu antecessor é '+n2+' ')
---
+++ AULA 1 - Introdução/atividade/scripts/calculadora.js
@@ -0,0 +1,5 @@
+var n1 = prompt('Quantas pessoas há na mesa?')
+var n2 = prompt('Qual o valor total da conta?')
+var div = Number(n2) / Number(n1)
+
+alert ('O valor de '+n2+' foi dividido entre '+n1+' pessoas e cada pagará '+div+' reais')
