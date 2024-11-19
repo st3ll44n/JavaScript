e5ede62369e5e5545024d742a159ecb45fb2aff4
st3ll44n <stellanaziozeno@gmail.com>
2024-11-19 14:26:06 -0300

    Update semana.js

--- AULA 2 - condicionais/atividade/scripts/semana.js
+++ AULA 2 - condicionais/atividade/scripts/semana.js
@@ -1,33 +1,35 @@
 
-       // Solicitar ao usuário um número de 1 a 7
-        prompt("Digite um número de 1 a 7 para saber o dia da semana: ");
-        numero = scanner.nextInt();
+function semana() {
+    const numero = parseInt(prompt("Digite um número de 1 a 7 para saber o dia da semana: "));
+    let dia;
 
-        // Verificar o número e exibir o dia correspondente
-        switch (numero) {
-            case "1":
-                alert("1 = Segunda-feira");
-                break
-            case "2":
-                ("2 = Terça-feira");
-                break
-            case "3":
-                alert("3 = Quarta-feira");
-                break
-            case "4":
-                alert("4 = Quinta-feira");
-                break
-            case "5":
-                alert("5 = Sexta-feira");
-                break
-            case "6":
-                alert("6 = Sábado");
-                break
-            case "7":
-                alert("7 = Domingo");
-                break
-            default:
-                alert("Número inválido! Por favor, insira um número de 1 a 7.");
-                break
-        }
-        scanner.close();
+    switch (numero) {
+        case 1:
+            dia = "Segunda-feira";
+            break;
+        case 2:
+            dia = "Terça-feira";
+            break;
+        case 3:
+            dia = "Quarta-feira";
+            break;
+        case 4:
+            dia = "Quinta-feira";
+            break;
+        case 5:
+            dia = "Sexta-feira";
+            break;
+        case 6:
+            dia = "Sábado";
+            break;
+        case 7:
+            dia = "Domingo";
+            break;
+        default:
+            dia = "Número inválido. Digite um número de 1 a 7.";
+    }
+
+    alert(`O número corresponde a: ${dia}`);
+}
+
+semana();
