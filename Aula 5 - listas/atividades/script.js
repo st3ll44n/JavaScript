c692d2bd94b4d4c2091954608250a07ea0c0c7f4
fulano <stellanaziozeno@gmail.com>
2024-11-25 14:27:48 -0300

    Atualização - Exercicio 5

--- Aula 5 - listas/atividades/script.js
+++ Aula 5 - listas/atividades/script.js
@@ -1,28 +1,24 @@
 function carregarCatalogo(){
 
-var filmes = [
-   ["aluzdodemonio.jpg","A luz do Demônio"]
-   ["halloween.jpg","Halloween"]
-   ["omalquenoshabita.jpg","O mal que nos habita"]
-   ["Rogai-Por-Nos.jpg","Rogai por nós"]
-   ["capadelivro4.jpg","It - A coisa"]
-//    ["",""]
-//    ["",""]
-//    ["",""]
-//    ["",""]
-//    ["",""]
- ]
-    filmes.forEach(cadafilmes =>{
-        document.getElementById('catalogo').innerHTML +=`
-        <div class="filmes">
-            <h3>${cadafilmes[1]}</h3>
-                <img src="${cadafilmes[0]}" alt="">
-            <button>
-                Adicionar
-                <img src="images/plus-lg.svg" alt="">
-            </button>
-        </div>
-            `
-        }
-        )}
+    var filmes = [
+        ["images/aluzdodemonio.jpg","A luz do Demônio"],
+        ["images/halloween.jpg","Halloween"],
+        ["images/omalquenoshabita.jpg","O mal que nos habita"],
+        ["images/Rogai-Por-Nos.jpg","Rogai por nós"],
+        ["images/capadelivro4.jpg","It - A coisa"]
+    ]
+        filmes.forEach(cadafilme =>{
+            document.getElementById('catalogo').innerHTML +=`
+            <div class="filme">
+                <h3>${cadafilme[1]}</h3>
+                    <img src="${cadafilme[0]}" alt="">
+                <button>
+                    Adicionar
+                    <img src="images/plus-lg.svg" alt="">
+                </button>
+            </div>
+                `
+            })
+    }
 
+carregarCatalogo()    
